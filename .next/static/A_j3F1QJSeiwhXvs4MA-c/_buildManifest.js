ec599918f5ed78d2f9581597642741aed6f944fb
fahim000i <mdfahimfaisal000@gmail.com>
2024-03-04 12:46:43 +0600

    .next

---
+++ .next/static/A_j3F1QJSeiwhXvs4MA-c/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/599-2bf8355fdd26141b.js","static/chunks/pages/index-611060b1e8f98b18.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
